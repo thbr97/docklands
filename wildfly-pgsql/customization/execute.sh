901fd6475bc1a3a56a94d193f630f5e34fef348f
Thorben Brinkmann <thorben@tcbweb.de>
2018-04-30 15:17:58 +0200

    Fixed some minor issues

--- wildfly-pgsql/customization/execute.sh
+++ wildfly-pgsql/customization/execute.sh
@@ -15,8 +15,21 @@ $JBOSS_HOME/bin/$JBOSS_MODE.sh -c $JBOSS_CONFIG >/dev/null &
 echo "=> Waiting for the server to boot"
 wait_for_server
 
-echo "=> Executing the commands"
-$JBOSS_CLI -c --file=`dirname "$0"`/commands.cli
+echo "=> Setup Datasource"
+$JBOSS_CLI -c << EOF
+batch
+# Add the postgrsql driver module
+module add --name=org.postgres --resources=/tmp/postgresql-${POSTGRESQL_VERSION}.jar --dependencies=javax.api,javax.transaction.api
+
+# Add Postgresql driver
+/subsystem=datasources/jdbc-driver=postgres:add(driver-name="postgres",driver-module-name="org.postgres",driver-class-name=org.postgresql.Driver)
+
+# Add the datasource
+data-source add --name=tcbwebDS --driver-name=postgres --jndi-name=java:/jdbc/tcbwebDS --connection-url=jdbc:postgresql://${DB_HOST}:5432/${DB_NAME}?useUnicode=true&amp;characterEncoding=UTF-8 --user-name=${DB_USER} --password=${DB_PASSWORD} --use-ccm=false --max-pool-size=25 --blocking-timeout-wait-millis=5000 --enabled=true
+
+run-batch
+EOF
+
 ${JBOSS_HOME}/bin/add-user.sh admin ${WILDFLY_ADMIN_PASSWORD}
 
 echo "=> Shutting down WildFly"
